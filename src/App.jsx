e25b9d9232d4615170331c1748536590d3a2d99e
niteshsingh1357 <nitesh.singh@gitstart.dev>
2024-06-27 07:35:40 +0545

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import "./App.css";
 import AddBook from "./components/addBook";
+import ErrorPage from "./components/errorPage";
 import Sidebar from "./components/sidebar";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<AddBook />} />
+        <Route path="/books" element={<AddBook />} />
+        {/* TODO: Add user path*/}
+
+        {/* 
+          Using path="*"" means "match anything", so this route
+          acts like a catch-all for URLs that we don't have explicit
+          routes for.
+        */}
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+function Layout() {
   return (
     <div className="container">
       {/* Divide screen horizontally into 2 parts: sidebar and main content */}
@@ -10,7 +31,7 @@ function App() {
         <Sidebar />
       </div>
       <div style={{ width: "80%" }} className="content">
-        <AddBook />
+        <Outlet />
       </div>
     </div>
   );
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,12 +1,13 @@
 import "./sidebar.css";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
   return(
     <div className="sidebar">
       <div className="sidebar-header">BMS</div>
       <div>
-        <p className="sidebar-item">Books</p>
-        <p className="sidebar-item">Users</p>
+        <NavLink to="/books" className="sidebar-item">Books</NavLink>
+        <NavLink to="/users" className="sidebar-item">Users</NavLink>
       </div>
     </div>
   )
